550a6fec7a4aaf146fedd23a8e443aafcd9d4d89
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-04 13:54:46 -0700

    Write a class Square that defines a square by: (based on 5-square.py)

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""class Square that defines a square based on previous square."""
+
+
+class Square:
+    """Private instance attribute called size."""
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size of square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size is not integer")
+        elif size < 0:
+            raise ValueError("size can't be less than zero")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        finds area of square
+        Returns:
+            the area of the square
+        """
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """method to return the size value."""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """method that sets the value size."""
+
+        if not isinstance(value, int):
+            raise TypeError("size is not int")
+        elif value < 0:
+            raise ValueError("Value can't be less than zero")
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """Method to returns the size value."""
+        if not isinstance(value, tuple):
+            raise TypeError("size not an int")
+        if len(value) != 2:
+            raise TypeError("not tuple of two 2 pos int")
+        if not isinstance(value[0], int):
+            raise TypeError("not a tuple of 2")
+        if not isinstance(value[1], int):
+            raise TypeError("not a tuple of 2")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("not a tuple of 2")
+        self.__position = value
+
+    def my_print(self):
+        """method to prints in stdout the square with the character."""
+
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
