17061851a97f56f27655b810d372efcabb3c49f3
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-04 03:34:01 -0700

    Write a class Square that defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Square that defines a square based on previous square."""
+
+
+class Square:
+    """Private instance attribute called size."""
+
+    def __init_-(self, size=0):
+        """
+        Args: 
+            size of square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size is integer")
+        elif size < 0:
+            raise ValueError("size can't be less than zero")
+        else:
+            self.__size = size
+
