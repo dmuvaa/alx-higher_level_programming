69e550cc60f16aac8a6af9fd16b4c6ab568cca87
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-07 09:55:29 -0700

    Write a class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -32,7 +32,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance (value, Node) and value is not None:
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
