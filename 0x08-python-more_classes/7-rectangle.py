98f37ef3652ebcf0412c3e1c4418015d64f7e428
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-10 14:17:31 -0700

    Write a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
             return rectangle
 
         for i in range(self.height):
-            rectangle += ("#" * self.width) + "\n"
+            rectangle += (str(self.print_symbol) * self.width) + "\n"
 
         return rectangle[:-1]
 
