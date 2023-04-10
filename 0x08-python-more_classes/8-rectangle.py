ebe28dab1b3894d155c2338cd1533d3bf84d464c
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-10 14:50:20 -0700

    Write a class Rectangle that defines a rectangle by: (based on 7-rectangle.py)

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Static method that returns the biggest rectangle based on the area."""
+        """Static method to return the biggest rectangle based on the area."""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
