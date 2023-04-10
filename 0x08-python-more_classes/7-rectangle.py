37a4ab7d136ee47659975c575c1de698aca33207
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-10 14:06:57 -0700

    Write a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """creates a class in called rectangle."""
 
     number_of_instances = 0
-    print_symbol = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Method to initialize width and height."""
