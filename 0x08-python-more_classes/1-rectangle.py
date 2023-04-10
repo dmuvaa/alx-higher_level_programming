53d58503bba7ea2fb53765de10613a5389ba7eed
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-10 07:17:07 -0700

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,24 +14,28 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    width.setter
+    @width.setter
 
     def width(self, value):
-        if not isinstance(int, width):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         elif width < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
         return self.__height
 
-    height.setter
+    @height.setter
 
     def height(self, value):
-        if not isinstance(int, height):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         elif height < 0:
             raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
         return self.__height
