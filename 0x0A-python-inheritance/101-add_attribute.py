2878b28ce92cb6ee27f66b7daf73e1326f795c27
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-11 14:15:58 -0700

    Write a function that adds a new attribute to an object if it’s possible

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Writes a function."""
+
+def add_attribute(obj, attr_name, attr_value):
+    """function that adds a new attribute to an object."""
+    if not hasattr(obj, "__setattr__") or
+    not callable(getattr(obj, "__setattr__")):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr_name, attr_value)
