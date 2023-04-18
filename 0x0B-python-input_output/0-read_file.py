5b606adc4451b8986cb4b0eec9b98c96e9a7a25e
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-18 10:25:58 -0700

    function that reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 
 def read_file(filename=""):
     """function that reads a text file."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end ="")
