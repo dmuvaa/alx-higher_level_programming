47a73663ed5a822130dc56adc0dd52fab910cb86
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-18 13:22:29 -0700

    script that adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,7 @@ from os import path
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
+
 def add_item(args: List[str]) -> None:
     """Function that adds all argument from a Pythhon List."""
     filename = "add_item.json"
@@ -18,6 +19,7 @@ def add_item(args: List[str]) -> None:
         items = []
     items += args[1:]
     save_to_json_file(items, filename)
+
 
 if __name__ == "__main__":
     add_item(sys.argv)
