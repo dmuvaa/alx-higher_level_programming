57c618bfaa948b4a23d3fffd12aff26485cd9d17
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-19 06:19:10 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Creates a Python Class."""
+
+
+class Student:
+    """class that defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Function that initializes a student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """public method that returns student dict to JSON."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Public Method to replace all attributes of the Student instance."""
+        for k, v in json.items():
+            setattr(self, k, v)
