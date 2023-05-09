904250b8e7deba24d387db8eca975652a0698f0e
Dennis Muvaa <dmuvaa70@gmail.com>
2023-05-09 06:28:55 -0700

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,3 +62,6 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.width * self.height
