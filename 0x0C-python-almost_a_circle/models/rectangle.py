0dc1f5fce23df8a001359e963c8e04f02417f592
Dennis Muvaa <dmuvaa70@gmail.com>
2023-05-09 06:42:59 -0700

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,7 @@ class Rectangle(Base):
         """method that displays # for rectangle."""
         for _ in range(self.height):
             print('#' * self.width)
+
+    def __str__(self):
+        """returns a string representation of the rectangle."""
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height)
