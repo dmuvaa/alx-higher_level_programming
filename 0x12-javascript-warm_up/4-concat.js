a62e20052040bbef14e680ec04642a67f56e7629
dmuvaa <dmuvaa70@gmail.com>
2023-06-12 18:01:24 +0300

    script that prints two arguments passed to it, in the following format: “ is ”

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,6 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if arg1 !== undefined && arg2 !== undefined) {
-	console.log(arg1 + ' is ' + arg2);
+if (arg1 !== undefined && arg2 !== undefined) {
+  console.log(arg1 + ' is ' + arg2);
 }
