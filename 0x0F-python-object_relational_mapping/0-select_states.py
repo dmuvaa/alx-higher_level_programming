4468d89f40e9aea6fcf79882f5126d8b9a94e1f3
dmuvaa <dmuvaa70@gmail.com>
2023-06-16 17:37:31 +0300

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,13 @@
 
 """Inherits from MySQLdb."""
 
-from MySQLdb import import MySQLdb
+import MySQLdb
 
-
-def __init__(self, "mysql username", "mysql password", "database name"):
-    self.username = "mysql username"
-    self.password = "mysql password"
-    self.name = "database name"
-
-    def __repr__(self):
-        return localhost:3306
+conn = MySQLdb.connect(host="localhost", port=3306, user="deno", passwd="password", db="hbtn_0e_0_usa", charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
