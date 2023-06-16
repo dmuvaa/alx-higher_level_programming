2de9fca7d173f659837d4c23645df3ff8e729232
dmuvaa <dmuvaa70@gmail.com>
2023-06-16 15:54:25 +0300

    script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Inherits from MySQLdb."""
+
+from MySQLdb import import MySQLdb
+
+
+def __init__(self, "mysql username", "mysql password", "database name"):
+    self.username = "mysql username"
+    self.password = "mysql password"
+    self.name = "database name"
+
+    def __repr__(self):
+        return localhost:3306
