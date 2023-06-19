8150a87bca646d2dbb25a0c6cf64a5eda08c3c37
dmuvaa <dmuvaa70@gmail.com>
2023-06-19 13:19:26 +0300

    Filter By State

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,20 +2,20 @@
 
 """imports a module."""
 
-
 import MySQLdb
 from sys import argv
 
-if __name__ == 'main':
+if __name__ == '__main__':
     """main."""
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
+    query = "SELECT * FROM states WHERE name LIKE BINARY \
+            '{}' ORDER BY id ASC".format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
