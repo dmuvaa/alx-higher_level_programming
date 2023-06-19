c74687a56706e560046f7336f04371d898d1e9dd
dmuvaa <dmuvaa70@gmail.com>
2023-06-19 17:45:11 +0300

    Write a script that lists all cities from the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,8 +13,13 @@ if __name__ == '__main__':
                            db=argv[3], charset="utf8")
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM cities ORDER BY id ASC")
+cur.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            JOIN states \
+            ON cities.state_id = states.id \
+            ORDER BY cities.id ASC")
 rows = cur.fetchall()
+
 for row in rows:
     print(row)
 cur.close()
