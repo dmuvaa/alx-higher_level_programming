bd272b8d33d4b40d4491a98c4bbd1fb677be14d2
dmuvaa <dmuvaa70@gmail.com>
2023-06-19 17:25:33 +0300

    Write a script that lists all cities from the database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""imports a module"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """main."""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2],
+                           db=argv[3], charset="utf8")
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM cities ORDER BY id ASC")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+conn.close()
