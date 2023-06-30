bdb9facb7829a655db66f172a8140198bbdf040e
dmuvaa <dmuvaa70@gmail.com>
2023-06-30 19:46:54 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/enc bash
+#!/bin/bash
 #script that taskes a url, sends a request to that URL, and displays the size of the body of the response
 curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
