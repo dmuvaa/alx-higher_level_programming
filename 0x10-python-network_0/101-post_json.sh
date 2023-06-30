4b1d70ab1937c4ba42a186113c003c4054d9f3d7
dmuvaa <dmuvaa70@gmail.com>
2023-06-30 23:56:39 +0300

    Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -X POST -H "Content-Type: application/json" -d @req.json http://localhost:9500
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
