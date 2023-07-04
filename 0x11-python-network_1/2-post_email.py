64223ae36500efe83dc3da37baaba10b0765ad8a
dmuvaa <dmuvaa70@gmail.com>
2023-07-04 18:59:52 +0300

    Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Imports a Module"""
+
+
+import sys
+import urllib.parse
+import urllib.request
+
+def send_post_request(url, email):
+    """creaates a func"""
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+
+
+if __name__ == "__main__":
+    send_post_request(sys.argv[1], sys.argv[2])
