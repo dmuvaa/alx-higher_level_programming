cbc44bef039af86da75e38eb5050ce106eab03f4
dmuvaa <dmuvaa70@gmail.com>
2023-07-04 19:12:39 +0300

    Write a Python script that takes in a URL  and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Imports a Module"""
+
+
+import sys
+import urllib.request
+import urllib.error
+
+
+def send_request(url):
+    """python script that takes a url"""
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    """main"""
+    send_request(sys.argv[1])
