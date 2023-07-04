e1c80fb1581c58ceaf940418e42a1134df50baad
dmuvaa <dmuvaa70@gmail.com>
2023-07-04 18:17:06 +0300

    Write a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,14 +6,14 @@ import urllib.request
 import sys
 
 
-def get_request_id(url):
-    with urllib.request.urlopen(url) as response:
+def get_request_id('https://alx-intranet.hbtn.io'):
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io') as response:
         print(response.headers.get('X-Request-Id'))
 
 
 def main():
     url = sys.argv[1]
-    get_request_id(url)
+    get_request_id('https://alx-intranet.hbtn.io')
 
 
 if __name__ == "__main__":
