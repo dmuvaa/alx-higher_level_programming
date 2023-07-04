fb6bb5dfd38dcc43d089c780b3552cdfac6786f3
dmuvaa <dmuvaa70@gmail.com>
2023-07-04 20:19:36 +0300

    Python script that takes 2 arguments in order to solve this challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,13 +2,10 @@
 
 """imports a module"""
 
-import urllib.request
+import requests
+import sys
 
-
-if __name__ == '__main__':
-    """main"""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- utf8 content:', html.decode('utf-8'))
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
