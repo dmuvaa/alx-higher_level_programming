9cca050c299819cce671c27fcde60dc8d3946a8e
dmuvaa <dmuvaa70@gmail.com>
2023-07-04 19:26:34 +0300

    Python Script That Fetches URL using Requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,3 @@ response = requests.get("https://alx-intranet.hbtn.io/status")
 print("Body response:")
 print("\t- type: {}".format(type(response.text)))
 print("\t- content: {}".format(response.text))
-
