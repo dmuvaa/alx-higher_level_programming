050d5da84cde239623de6c76683ddd126965159f
dmuvaa <dmuvaa70@gmail.com>
2023-07-04 19:46:47 +0300

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,7 @@ import sys
 
 
 if __name__ = '__main__':
+    """main"""
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print('Error code: {}'.format(response.status_code))
