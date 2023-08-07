3815d3df2fd7b4a332a800cae8f9b9810e25211b
Dennis Muvaa <dmuvaa70@gmail.com>
2023-08-07 12:34:18 +0300

    JavaScript script that updates the text color of the <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+// First, ensure that the jQuery library is loaded and the document is ready
+$(document).ready(function() {
+    // Use jQuery to select the <header> element and change its text color to red
+    $("header").css("color", "#FF0000");
+});
+
